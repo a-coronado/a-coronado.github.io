6a6e184612386ce3d2a9b40ac0d3a9610f0b13b0
Alejandra Coronado <149135840+a-coronado@users.noreply.github.com>
2023-11-29 04:58:34 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #e07a5f;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Estudio Ingenieria en Administrador de Sistema.</span>')
+  .typeString('<span style="color: #e07a5f;">Estudio Ingenieria en Administrador de Sistema.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
